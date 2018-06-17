8663ea3ac03f3ae0ce059ea12f73b677ebcbb20b
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-17 10:43:35 +0530

    first commit

---
+++ src/main/java/ca/demo/salesTool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salesTool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+	}
+ 
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+	}
+}
