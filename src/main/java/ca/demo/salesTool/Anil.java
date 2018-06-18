2a05060dcfdeae661341371531e0a2fa85fc752c
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-18 07:30:25 +0530

    master branch with stepB commit

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -4,5 +4,8 @@ public class Anil {
  public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("hi how are you");
+	for(int i=1; i<=2; i++) {
+		System.out.println("*");
+	}
 }
 }
