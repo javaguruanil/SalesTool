952f7a0e1937d23e674024a3dfc4c27087584408
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-18 07:50:46 +0530

    repo commit

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -3,10 +3,5 @@ package ca.demo.salesTool;
 public class Anil {
  public static void main(String[] args) {
 	System.out.println("hello");
-	System.out.println("hi how are you");
-	for(int i=1; i<=2; i++) {
-		System.out.println("*");
-	} 
-	System.out.println("HELLO");
-} 
 }
+} 
