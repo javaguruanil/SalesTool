b5a8381b8ba1389d6f56ed87df942738c3dfd0cd
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-18 07:35:18 +0530

    master branch commit

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -4,8 +4,5 @@ public class Anil {
  public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("hi how are you");
-	for(int i=1; i<=2; i++) {
-		System.out.println("*");
-	}
 }
 }
