9e53be4ff37b585c62e0e4c58fa33b3f81079634
BH06278 <Anil.Mohandu@macys.com>
2018-06-25 21:32:57 +0530

    displayData branch

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -3,6 +3,6 @@ package ca.demo.salesTool;
 public class Anil {
  public static void main(String[] args) {
 	System.out.println("hello");
-	System.out.println("hi how are you");
+	System.out.println("hi how are you"); 
 }
 }
