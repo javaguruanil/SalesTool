bc48799ccf8c1d2c9e597b86a4db5758701897c9
BH06278 <Anil.Mohandu@macys.com>
2018-06-25 21:22:00 +0530

    Added new lines in master

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -3,5 +3,16 @@ package ca.demo.salesTool;
 public class Anil {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hi how are you");
+		for (int i = 1; i <= 2; i++) {
+			System.out.println("*");
+		}
+		System.out.println("BLAKE ");
+	}
+
+	static void m11() {
+	}
+
+	static void m22() {
 	}
 }
