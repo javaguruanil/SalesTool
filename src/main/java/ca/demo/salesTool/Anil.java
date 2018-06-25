6909420559580183a351f77e55331cde4899335f
BH06278 <Anil.Mohandu@macys.com>
2018-06-25 21:27:21 +0530

    adffdfd

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -3,19 +3,5 @@ package ca.demo.salesTool;
 public class Anil {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("hi how are you");
-		for (int i = 1; i <= 2; i++) {
-			System.out.println("*");
-		}
-		System.out.println("BLAKE ");
 	}
-<<<<<<< Upstream, based on origin/master
-
-	static void m11() {
-	}
-
-	static void m22() {
-	}
-=======
->>>>>>> 12516ae commit from ABCRepo1
 }
--- src/main/java/ca/demo/salesTool/Raju.java
+++
@@ -1,7 +0,0 @@
-package ca.demo.salesTool;
-
-public class Raju {
-  void m1() {
-   System.out.println("Raju Mohandu Anil");	  
-  }
-}
