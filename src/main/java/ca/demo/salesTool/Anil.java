d34299c4c5fafa52e4d1272ceea7070c6e7638c9
BH06278 <Anil.Mohandu@macys.com>
2018-06-25 21:09:44 +0530

    Latest master

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -1,18 +1,7 @@
 package ca.demo.salesTool;
 
 public class Anil {
- public static void main(String[] args) {
-	System.out.println("hello");
-	System.out.println("hi how are you");
-	for(int i=1; i<=2; i++) {
-		System.out.println("*");
-	} 
-	System.out.println("BLAKE ");
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
- static void m2() {
-	 System.out.println("hi");
- }
- static void m3() {
-	 System.out.println("hello");
- }
-} 
