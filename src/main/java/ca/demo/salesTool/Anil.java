c4f77e467bf1602d75bb0881d24f13cfafdd5145
BH06278 <Anil.Mohandu@macys.com>
2018-06-26 10:21:27 +0530

    master commit

--- src/main/java/ca/demo/salesTool/Anil.java
+++ src/main/java/ca/demo/salesTool/Anil.java
@@ -2,9 +2,6 @@ package ca.demo.salesTool;
 
 public class Anil {
 	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-	void m1() {
-		System.out.println("f...");
+		System.out.println("hi");
 	}
 }
